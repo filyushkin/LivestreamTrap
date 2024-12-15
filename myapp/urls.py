5fa521c4a3f3cd7c2ac952e0fe49371baa02cc46
Dmitry Filyushkin <dmfi@protonmail.com>
2024-12-15 19:59:43 +0300

    >>>

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('myform/', my_view, name='my_form'),
     path('main/', views.my_view, name='main'),
     path('tasks/', views.tasks, name='tasks'),
     path('downloads/', views.downloads, name='downloads')
