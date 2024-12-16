ea89bf9737644e3fa970abfed6b89e74e84e1ca3
Yolshin <elshin195@gmail.com>
2024-12-16 20:00:25 +0700

    bugfix

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'huey.contrib.djhuey',
+    'huey.contrib.djhuey',  # myapp после djhuey
     'myapp',
 ]
 """
