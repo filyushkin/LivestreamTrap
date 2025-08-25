59ad04c78dd2d0a30deb231f24d3b7c954f494d1
Yolshin <elshin195@gmail.com>
2025-08-25 19:48:43 +0700

    added use .env

--- LivestreamTrap/settings.py
+++ LivestreamTrap/settings.py
@@ -13,8 +13,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Загружаем переменные из .env файла
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -158,3 +163,6 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 USE_THOUSAND_SEPARATOR = True
+
+# YouTube settings
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
